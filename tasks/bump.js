568ff96a0f924d2cec9760b0e9037ae4e37660d3
Jakub Nesnídal <arakir@seznam.cz>
2014-03-30 03:10:32 +0200

    feat: switch git to svn

--- tasks/bump.js
+++ tasks/bump.js
@@ -2,7 +2,7 @@
  * Increase version number
  *
  * grunt bump
- * grunt bump:git
+ * grunt bump:svn
  * grunt bump:patch
  * grunt bump:minor
  * grunt bump:major
@@ -10,13 +10,15 @@
  * @author Vojta Jina <vojta.jina@gmail.com>
  * @author Mathias Paumgarten <mail@mathias-paumgarten.com>
  * @author Adam Biggs <email@adambig.gs>
+ * @author Jakub Nesnídal <arakir@seznam.cz>
  */
 var semver = require('semver');
+var svn_info = require('svn-info');
 var exec = require('child_process').exec;
 
 module.exports = function(grunt) {
 
-  var DESC = 'Increment the version, commit, tag and push.';
+  var DESC = 'Increment the version, commit and tag';
   grunt.registerTask('bump', DESC, function(versionType, incOrCommitOnly) {
     var opts = this.options({
       bumpVersion: true,
@@ -28,9 +30,7 @@ module.exports = function(grunt) {
       createTag: true,
       tagName: 'v%VERSION%',
       tagMessage: 'Version %VERSION%',
-      push: true,
-      pushTo: 'upstream',
-      gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d'
+      tagLocation: ''
     });
 
     if (incOrCommitOnly === 'bump-only') {
@@ -38,11 +38,10 @@ module.exports = function(grunt) {
 
       opts.commit = false;
       opts.createTag = false;
-      opts.push = false;
     }
 
     if (incOrCommitOnly === 'commit-only') {
-      grunt.verbose.writeln('Only commiting/taggin/pushing.');
+      grunt.verbose.writeln('Only commiting/taggin.');
 
       opts.bumpVersion = false;
     }
@@ -67,19 +66,23 @@ module.exports = function(grunt) {
     };
 
     var globalVersion; // when bumping multiple files
-    var gitVersion;    // when bumping using `git describe`
+    var svnVersion;    // when bumping using `svn info`
     var VERSION_REGEXP = /([\'|\"]?version[\'|\"]?[ ]*:[ ]*[\'|\"]?)([\d||A-a|.|-]*)([\'|\"]?)/i;
 
+    // GET SVN INFO
+    var svnInfo;
+    var getSvnInfo = function(prop){
+      if (!svnInfo) {
+        svnInfo = svn_info.sync();
+      }
+      return prop ? svnInfo[prop] : svnInfo;
+    };
 
-    // GET VERSION FROM GIT
-    runIf(opts.bumpVersion && versionType === 'git', function(){
-      exec('git describe ' + opts.gitDescribeOptions, function(err, stdout, stderr){
-        if (err) {
-          grunt.fatal('Can not get a version number using `git describe`');
-        }
-        gitVersion = stdout.trim();
-        next();
-      });
+
+    // GET VERSION FROM SVN
+    runIf(opts.bumpVersion && versionType === 'svn', function(){
+      svnVersion = svn_info.sync(getSvnInfo('url'))['revision'];
+      next();
     });
 
 
@@ -88,8 +91,8 @@ module.exports = function(grunt) {
       opts.files.forEach(function(file, idx) {
         var version = null;
         var content = grunt.file.read(file).replace(VERSION_REGEXP, function(match, prefix, parsedVersion, suffix) {
-          gitVersion = gitVersion && parsedVersion + '-' + gitVersion;
-          version = exactVersionToSet || gitVersion || semver.inc(parsedVersion, versionType || 'patch');
+          svnVersion = svnVersion && parsedVersion + '-' + svnVersion;
+          version = exactVersionToSet || svnVersion || semver.inc(parsedVersion, versionType || 'patch');
           return prefix + version + suffix;
         });
 
@@ -140,7 +143,7 @@ module.exports = function(grunt) {
     runIf(opts.commit, function() {
       var commitMessage = opts.commitMessage.replace('%VERSION%', globalVersion);
 
-      exec('git commit ' + opts.commitFiles.join(' ') + ' -m "' + commitMessage + '"', function(err, stdout, stderr) {
+      exec('svn commit ' + opts.commitFiles.join(' ') + ' -m "' + commitMessage + '"', function(err, stdout, stderr) {
         if (err) {
           grunt.fatal('Can not create the commit:\n  ' + stderr);
         }
@@ -151,27 +154,36 @@ module.exports = function(grunt) {
 
 
     // CREATE TAG
-    runIf(opts.createTag, function() {
+    runIf(opts.createTag, function(){
       var tagName = opts.tagName.replace('%VERSION%', globalVersion);
       var tagMessage = opts.tagMessage.replace('%VERSION%', globalVersion);
+      var toUrl;
+      var fromUrl = getSvnInfo('url');
+      var tagLocation = opts.tagLocation.replace(/\/$/,'');
 
-      exec('git tag -a ' + tagName + ' -m "' + tagMessage + '"' , function(err, stdout, stderr) {
+
+      if (!tagLocation) {
+        grunt.verbose.writeln('Trying to find tags location from: ' + fromUrl);
+        ['trunk','branches'].some(function(dir){
+          tagLocation = fromUrl.substring(0,fromUrl.lastIndexOf(dir));
+          tagLocation += tagLocation && 'tags';
+          return tagLocation;
+        });
+      }
+
+      grunt.verbose.writeln('Resolved tags location: ' + tagLocation);
+
+      if (!tagLocation) {
+        grunt.fatal('Can not resolve tags location');
+      }
+
+      toUrl = tagLocation + '/' + tagName;
+
+      exec('svn copy "' + fromUrl + '" "' + toUrl + '" -m "' + tagMessage + '"' , function(err, stdout, stderr) {
         if (err) {
           grunt.fatal('Can not create the tag:\n  ' + stderr);
         }
         grunt.log.ok('Tagged as "' + tagName + '"');
-        next();
-      });
-    });
-
-
-    // PUSH CHANGES
-    runIf(opts.push, function() {
-      exec('git push ' + opts.pushTo + ' && git push ' + opts.pushTo + ' --tags', function(err, stdout, stderr) {
-        if (err) {
-          grunt.fatal('Can not push to ' + opts.pushTo + ':\n  ' + stderr);
-        }
-        grunt.log.ok('Pushed to ' + opts.pushTo);
         next();
       });
     });
